dd1b264fb4172f1e06ea3b3a9f46b14e960ebf23
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-06-24 14:08:12 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDrg-BnPUzlgmOBRym61rqRDGGBSKH3h6M",
@@ -17,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
