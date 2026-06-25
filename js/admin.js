58193e40afa51d74c1fea0ebaa421bb90715388b
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-06-25 15:42:48 -0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -480,7 +480,12 @@ card.innerHTML = `
     <div class="pedido-header">
 
         <div>
-            <h3>👤 ${pedido.nombreCliente}</h3>
+        <h3>
+        👤 ${
+        pedido.nombreCliente ||
+        pedido.correo
+        }
+        </h3>
         </div>
 
         <span class="estado estado-${pedido.estado}">
